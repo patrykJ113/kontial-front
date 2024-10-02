43e72cc232ee01436c1ef9d437338573453ed3cb
Patryk <patryk.d.janus@gmail.com>
2024-10-02 14:04:28 +0200

    create person service

---
+++ src/app/service/person/person.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PersonService {
+  private apiUrl = 'http://localhost:8080/api/persons';
+
+  constructor(private http: HttpClient) {}
+
+  getPersons(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
